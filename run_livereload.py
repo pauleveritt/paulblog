d7525ffaa712fcc83dfd6ad499ed7e8689f40fab
Paul Everitt <pauleveritt@me.com>
2018-04-01 17:01:55 -0400

    Add livereload.

---
+++ run_livereload.py
@@ -0,0 +1,20 @@
+"""
+
+Automatically re-build Sphinx docs while editing, serve docs on an
+HTTP port, and also reload any browsers pointed to the docs.
+
+"""
+
+from livereload import shell
+
+from kaybee_bulma.run_livereload import get_server
+
+sphinx = "env3/bin/python3 env3/bin/sphinx-build -E -b html docs docs/_build"
+
+if __name__ == '__main__':
+    server = get_server()
+    server.watch('docs/**', shell(sphinx),
+                 ignore=lambda s: '_build' in s)
+    server.watch('kaybee_bulma/**/*.html', shell(sphinx))
+    server.watch('kaybee_bulma/**.py', shell(sphinx))
+    server.serve(root='docs/_build', live_css=False)
